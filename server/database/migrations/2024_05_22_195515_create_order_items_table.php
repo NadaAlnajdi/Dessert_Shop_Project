7038f291a8d594c8a9e3e6800200abbeb1ec08cf
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-05-23 23:47:57 +0300

    Update Migration Files

--- server/database/migrations/2024_05_22_195515_create_order_items_table.php
+++ server/database/migrations/2024_05_22_195515_create_order_items_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('order_items', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('order_id'); 
             $table->unsignedBigInteger('product_id'); 
             $table->integer('quantity'); 
