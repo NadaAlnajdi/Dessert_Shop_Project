bfd86d26ef056c815652257ab65aa34e5501d101
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-05-27 20:34:44 +0300

    "handle relations between Wishlist,Product, user"

--- server/app/Models/Product.php
+++ server/app/Models/Product.php
@@ -30,8 +30,9 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
-    public function wishlist()
+    public function wishlists()
     {
         return $this->belongsToMany(Wishlist::class);
     }
+
 }
