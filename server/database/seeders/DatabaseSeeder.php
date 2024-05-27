390e6cb0c0a7be3458145b54fdeedc6c59c0d529
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-05-27 20:37:39 +0300

    Add user factory and seeder to create a fake users data

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
             PromotionSeeder::class,
