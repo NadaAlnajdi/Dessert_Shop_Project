264e2064d674b801d325a898e3175339d77d2a73
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-05-29 01:23:48 +0300

    "add checkout component and handle routes"

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {path:'',component:WishlistComponent}
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'wishlist', component: WishlistComponent },
+    {path:'**',redirectTo:'',pathMatch:'full'},
+
 ];
