eba855e982b576203a028095bc68b9bbfaa837aa
fatmamahmoud30114 <fatmamahmoud30114@gmail.com>
2024-05-29 23:07:20 +0200

    userprofile

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserProfileComponent } from './components/userprofile/userprofile.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'',component:UserProfileComponent},
     {path:'home',component:HomeComponent},
     { path: 'checkout', component: CheckoutComponent },
     { path: 'wishlist', component: WishlistComponent },
