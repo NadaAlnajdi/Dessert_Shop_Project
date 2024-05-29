a87928a577826b40ba6924618de6a67895253be4
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-05-29 21:27:14 +0300

    "add environments"

--- client/src/app/services/wishlist.service.ts
+++ client/src/app/services/wishlist.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WishlistService {
-  private baseUrl = 'http://127.0.0.1:8000/wishlists/';
+  private apiUrl = `${environment.apiUrl}/wishlists`;
 
+  
   constructor(private http: HttpClient) { }
 
   getWishlist(): Observable<any[]> {
-    return this.http.get<any[]>(this.baseUrl);
+    return this.http.get<any[]>(this.apiUrl);
   }
 
   removeFromWishlist(productId: number): Observable<void> {
-    return this.http.delete<void>(this.baseUrl+productId);
+    return this.http.delete<void>(`${this.apiUrl}/${productId}`);
   }
 }
