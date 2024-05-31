aa5359a5b7068fc04d1734340beb7ab9dac14536
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-06-01 01:53:06 +0300

    Update admin dashboard categories page

--- client/src/app/components/admin-dashboard/categories/categories.component.ts
+++ client/src/app/components/admin-dashboard/categories/categories.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardService } from '../../../services/admin-dashboard.service';
-import { Category } from '../../../interfaces/category';
 
 @Component({
   selector: 'app-categories',
