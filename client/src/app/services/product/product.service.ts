e4c204068230841e55d6243219a4eb4efdbfcc76
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-05-31 18:12:05 +0300

    Update Product Service

--- client/src/app/services/product/product.service.ts
+++ client/src/app/services/product/product.service.ts
@@ -1,3 +1,4 @@
+// product.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -9,7 +10,7 @@ import { environment } from '../../../environments/environment';
 export class ProductService {
   private apiUrl = `${environment.apiUrl}/products`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProducts(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
