c52657a2d5e31eeeaa7d2d70787c274cd157d095
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-06-01 01:42:11 +0300

    Add admin dashboard categories page

---
+++ client/src/app/components/admin-dashboard/categories/categories.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminDashboardService } from '../../../services/admin-dashboard.service';
+import { Category } from '../../../interfaces/category';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css',
+})
+export class CategoriesComponent implements OnInit {
+  allCategories!: any[];
+
+  constructor(private adminDashboardService: AdminDashboardService) {}
+
+  ngOnInit(): void {
+    this.fetchCategoriesData();
+  }
+
+  fetchCategoriesData(): void {
+    this.adminDashboardService.getCategoriesData().subscribe(
+      (data) => {
+        console.log(data);
+        this.allCategories = data;
+      },
+      (error) => {
+        console.error('Error fetching Users data', error);
+      }
+    );
+  }
+}
